623b4b1b63126f95b822ead8fff67737ac73d6f9
Tibsmith <tobybsmith121@gmail.com>
2020-01-04 04:56:20 -0500

    have player and world

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+
+func _ready():
+	set_process(true)
+
+func _physics_process(delta):
+	_get_input(delta)
+	move_and_slide(velocity)
+
+func _get_input(delta):
+	if(Input.is_action_pressed("ui_left")):
+		velocity.x -= 10
+		if(velocity.x < -300):
+			velocity.x = -300
+	if(Input.is_action_pressed("ui_right")):
+		velocity.x += 10
+		if(velocity.x > 300):
+			velocity.x = 300
+	if(Input.is_action_pressed("ui_up")):
+		velocity.y -= 10
+		if(velocity.y < -300):
+			velocity.y = -300
+	if(Input.is_action_pressed("ui_down")):
+		velocity.y += 10
+		if(velocity.y > 300):
+			velocity.y = 300
