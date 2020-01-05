e6df3a29b8891cc926f871e3376aa65f19e0269b
Tibsmith <tobybsmith121@gmail.com>
2020-01-05 06:48:10 -0500

    better player movemennt but not perfect

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
+var pressed = false
 
 func _ready():
 	set_process(true)
 
 func _physics_process(delta):
 	_get_input(delta)
+	print(pressed)
+	if pressed == false:
+		velocity.x = 0
+		velocity.y = 0
 	move_and_slide(velocity)
 
 
@@ -14,18 +19,16 @@ func _physics_process(delta):
 #deceleration and stopping
 func _get_input(delta):
 	if(Input.is_action_pressed("ui_left")):
-		velocity.x -= 10
-		if(velocity.x < -300):
-			velocity.x = -300
+		pressed = true
+		velocity.x = -300
 	if(Input.is_action_pressed("ui_right")):
-		velocity.x += 10
-		if(velocity.x > 300):
-			velocity.x = 300
+		pressed = true
+		velocity.x = 300
 	if(Input.is_action_pressed("ui_up")):
-		velocity.y -= 10
-		if(velocity.y < -300):
-			velocity.y = -300
+		pressed = true
+		velocity.y = -300
 	if(Input.is_action_pressed("ui_down")):
-		velocity.y += 10
-		if(velocity.y > 300):
-			velocity.y = 300
+		pressed = true
+		velocity.y = 300
+	if not(Input.is_action_pressed("ui_down")) and not (Input.is_action_pressed("ui_up")) and not (Input.is_action_pressed("ui_right")) and not (Input.is_action_pressed("ui_left")):
+		pressed = false;
